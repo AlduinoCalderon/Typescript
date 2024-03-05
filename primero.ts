3dd591d51bdc787004787a9aaf08fe0aa7b35f48
Alduin <aldocalderon97@gmail.com>
2024-03-05 10:10:01 -0600

    tslint

---
+++ primero.ts
@@ -0,0 +1,2 @@
+export const nombreMateria = "SOA";
+console.log(nombreMateria);
---
+++ salida/primero.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombreMateria = void 0;
+exports.nombreMateria = "SOA";
+console.log(exports.nombreMateria);
+//# sourceMappingURL=primero.js.map
